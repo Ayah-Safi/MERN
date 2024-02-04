97b18f2270c439157b4dd5142ab327139c0a47fd
Ayah Safi <ayahsafi@Ayahs-MacBook-Pro.local>
2024-02-04 14:18:38 +0200

    Predict Destructuring Assignment Upload

---
+++ Javascript/PredictDestructuring.js
@@ -0,0 +1,64 @@
+//1. Predict the Output for Problem:
+// Tesla
+// Mercedes
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//2. Predict the Output for Problem:
+// name is not defined
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//3. Predict the Output for Problem:
+// 12345
+// error, not defined passworf for person
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// Predict the Output for Problem:
+// false
+// true
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//5.Predict the Output for Problem:
+// vlaue
+// [1,5,1,8,3,,3]
+// [1]
+// [5]
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
